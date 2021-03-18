f2ca8c8fac3c323c414b91fe2caa101e304e5071
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-03-18 14:08:01 -0700

    Section 1.7 Program 1-2
    practice the use \n

---
+++ Program 1-2/main.cpp
@@ -0,0 +1,10 @@
+//This program prints a message with my name in it.
+#include <iostream>
+using std::cout;
+
+int main()
+{
+    cout << "Hi its me \n";
+    cout << "I am learning to program! \n";
+    return 0;
+}
