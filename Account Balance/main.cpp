f115a426d06a316160b7af07ce1a18ed72dcc905
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-03-18 14:21:54 -0700

    final program no loop

--- Account Balance/main.cpp
+++ Account Balance/main.cpp
@@ -8,5 +8,29 @@ using std::endl;
 
 int main()
 {
-    
+    double startingBalance;
+    double totalDeposit;
+    double takeAmount;
+    double Total;
+
+    cout << "What is you starting balance on the Account" << endl;
+    cout << "$";
+    cin >> startingBalance;
+
+    cout << "Thank you for that information" << endl;
+    cout << "Next, Please provide me the dollar amount of the current deposit:" << endl;
+    cout << "$";
+    cin >> totalDeposit;
+
+    cout << "Thank you, finally provide me the dollar amount that will be withdrawn" << endl;
+    cout << " and I will calculate the Account Balance. " << endl;
+    cout << "$";
+    cin >> takeAmount;
+
+    Total = (startingBalance + totalDeposit - takeAmount);
+
+    cout << "The account Balance is $" << Total << endl;
+    cout << "Thank you for using the program" << endl;
+
+    return 0;
 }
