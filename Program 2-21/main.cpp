6a49fafaa6bdee1614675117df01fc7ac92afb80
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-03-19 15:29:22 -0700

    created the varables

---
+++ Program 2-21/main.cpp
@@ -0,0 +1,22 @@
+//This program covers seconds to minutes to hours to minutes
+//to seconds. It uses integer divisions and the modulus operator.
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    double totalSeconds;
+
+    double years;
+    double months;
+    double weeks;
+    double days;
+    double hours;
+    double minutes;
+    double seconds;
+
+    
+}
