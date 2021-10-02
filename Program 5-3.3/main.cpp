8cac894abbc1c488967b798fd2781678b472546a
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-10-02 14:57:37 -0700

    to main

--- Program 5-3.3/main.cpp
+++ Program 5-3.3/main.cpp
@@ -20,7 +20,37 @@ int main()
     int players,
         teamSize,
         numTeams,
-        leftOver,         
-        candy bar;
+        leftOver;
+
+    cout << "How many players do you wish per team\n";
+    cout << "(Enter a value in the range " << MIN_PLAYERS << " - " << MAX_PLAYERS << "): ";
+    cin >> teamSize;  
+
+    //validate the input from the user
+    while(teamSize < MIN_PLAYERS || teamSize > MAX_PLAYERS)
+    {
+        cout << "\nTeam size should be " << MIN_PLAYERS << " to " << MAX_PLAYERS << " players. \n";
+        cout << "How manu players do you wish per team? ";
+        cin >> teamSize; 
+    }   
+    //get and validate the number of players avalible
+    cout << "\nHow manu players are avalible? ";
+    cin >> players;
+    
+    //checks if the number is possitive
+    while (players <=0)
+    {
+        cout << "Please enter a possitive number: ";
+        cin >> players;
+    }
+
+    //calculate the number of teams and number of leftover players
+    numTeams = players / teamSize;
+    leftOver = players % teamSize;
+
+    //dispay the results 
+    cout << "\nThere will be " << numTeams << " teams with ";
+    cout << leftOver << " players left over without a team.\n";
+
     return 0;
 }
