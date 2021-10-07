80f012cf71ed1045ffc793f28bedcadd13d3838c
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-10-06 23:56:02 -0700

    practice finished

---
+++ Program 5-16/main.cpp
@@ -0,0 +1,33 @@
+/******************************************************************************* 
+** Author:       Ruben Sanduleac
+** Date:         October 6th, 2021
+** Description:  This program writes data to a file.
+*******************************************************************************/ 
+#include <iostream>
+#include <fstream>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::ofstream;
+
+int main()
+{
+    //file variable
+    ofstream outputFile;
+
+    //open the output file
+    outputFile.open("demofile.txt");
+
+    
+    //Write four names to the file
+    outputFile << "Bach\n";
+    outputFile << "Beethoven\n";
+    outputFile << "Mozart\n";
+    outputFile << "Schubert\n";
+
+    //Close file
+    outputFile.close();
+
+    cout << "Done.\n";
+    return 0;
+}
