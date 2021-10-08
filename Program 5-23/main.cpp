a6c3aac1374e32cb125a45fd7ad844738ffe4f79
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-10-07 22:26:29 -0700

    finished

---
+++ Program 5-23/main.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************* 
+** Author:       Ruben Sanduleac
+** Date:         October 6th, 2021
+** Description:  This program produces a loan amortization table for the
+**               Central Mountain Credit Union.
+*******************************************************************************/ 
+#include <iostream>
+#include <cmath>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    double loan,
+           rate,
+           moInterestRate,
+           years,
+           balance,
+           term,
+           payment;
+
+    int    numPayments;
+
+    
+    return 0;
+}
