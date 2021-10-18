1971a52fed4120dbd7da149e043eb254e9839887
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-10-17 23:59:42 -0700

    added the functions

--- Program 6-28/main.cpp
+++ Program 6-28/main.cpp
@@ -4,6 +4,36 @@
 ** Description:  Using a static Variable 
 *******************************************************************************/ 
 #include <iostream>
+#include <iomanip>
 using std::cout;
 using std::cin;
 using std::endl;
+
+int square(int);
+double square(double);
+
+int main()
+{
+    int userInt;
+    double userReal;
+
+    cout << "Enter an integer and a floating - point value: ";
+    cin >> userInt >> userReal;
+
+
+    //display their squares
+    cout << "Here are their squares: ";
+    cout << std::fixed << std::showpoint << std::setprecision(2);
+    cout << square(userInt) << " and " << square(userReal) << endl;
+
+    return 0;
+}
+
+int square(int)
+{
+
+}
+double square(double)
+{
+    
+}
