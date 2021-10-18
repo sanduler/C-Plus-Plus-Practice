5f8a9dd7475e144ac1a731e11b877d6e4051de59
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-10-18 00:37:17 -0700

    Create main.cpp

---
+++ Program 7-4/main.cpp
@@ -0,0 +1,15 @@
+/******************************************************************************* 
+** Author:       Ruben Sanduleac
+** Date:         October 17th, 2021
+** Description:  This program shows how to use the constructor
+*******************************************************************************/ 
+#include <iostream>
+#include <iomanip>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    return 0;
+}
