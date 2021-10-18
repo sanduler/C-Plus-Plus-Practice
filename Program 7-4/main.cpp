905a0087bda366cdcc2c23cbec8f851b5d076388
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-10-18 00:39:40 -0700

    finished 7/15

--- Program 7-4/main.cpp
+++ Program 7-4/main.cpp
@@ -9,7 +9,30 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+class Demo
+{
+private:
+    /* data */
+public:
+    Demo(/* args */);
+    ~Demo();
+};
+
+Demo::Demo(/* args */)
+{
+    cout << "Now the constructor is running!" << endl;
+}
+
+Demo::~Demo()
+{
+}
+
 int main()
 {
+    cout << "This is displayed before the object is created \n";
+
+    Demo demoObj;
+
+    cout << "This is displayed after the object is created.\n";
     return 0;
 }
