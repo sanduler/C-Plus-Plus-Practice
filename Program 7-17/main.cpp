46ae9a097a3a2bb3c9cf1ecc7dd57ecb568d438b
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-10-24 21:20:02 -0700

    push the main

---
+++ Program 7-17/main.cpp
@@ -0,0 +1,18 @@
+/* *******************************************************************************
+ * Name: Ruben Sanduleac
+ * Date: October 24th, 2021
+ * Description: This client program uses the Account class to perform simple
+ *              banking operations. This file should be combined into a project
+ *              along with the Account.h and Account.cpp files.
+ * ******************************************************************************/
+#include<iostream>
+#include<iomanip>
+#include "Account.h"
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    return 0;
+}
