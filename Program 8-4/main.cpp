b532105f4f2490a311d99f777596bcfd2fe381f5
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-02 17:03:01 -0800

    finished

--- Program 8-4/main.cpp
+++ Program 8-4/main.cpp
@@ -10,20 +10,20 @@ using namespace std;
 int main()
 {
     const int NUM_MONTHS = 12;
-    int days[NUM_MONTHS];
+    int days[NUM_MONTHS] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    days[0] = 31;  // January
-    days[1] = 28;  // February
-    days[2] = 31;  // March
-    days[3] = 30;  // April
-    days[4] = 31;  // May
-    days[5] = 30;  // June
-    days[6] = 31;  // July
-    days[7] = 31;  // August
-    days[8] = 30;  // September
-    days[9] = 31;  // October
-    days[10] = 30; // November
-    days[11] = 31; // December
+    // days[0] = 31;  // January
+    // days[1] = 28;  // February
+    // days[2] = 31;  // March
+    // days[3] = 30;  // April
+    // days[4] = 31;  // May
+    // days[5] = 30;  // June
+    // days[6] = 31;  // July
+    // days[7] = 31;  // August
+    // days[8] = 30;  // September
+    // days[9] = 31;  // October
+    // days[10] = 30; // November
+    // days[11] = 31; // December
 
     for (int month = 0; month < NUM_MONTHS; month++)
     {
