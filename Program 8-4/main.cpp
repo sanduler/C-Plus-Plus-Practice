906daa0f27776f0a9273f7525bc7b6e8918fdd07
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-02 16:50:57 -0800

    finished

---
+++ Program 8-4/main.cpp
@@ -0,0 +1,16 @@
+/*
+ *   Name: Ruben Sanduleac
+ *   Date: January 2, 2022
+ *   Description: This program dispalys the number of days in each month
+ */
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    const int NUM_MONTHS = 12;
+    int days[NUM_MONTHS];
+
+        return 0;
+}
